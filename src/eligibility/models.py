c78f9d9dc58d30a3e7d8f0c0c99ff8a6dbddfa84
Przemysław Górecki <przemyslaw@toptal.com>
2020-08-13 22:35:40 +0200

    model-level eligibility queue functionality

--- src/eligibility/models.py
+++ src/eligibility/models.py
@@ -1,5 +1,8 @@
 import uuid
 from django.db import models
+from django.db.models import Q
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from simple_history.models import HistoricalRecords
 from agency.models import Agency
 from client.models import Client
@@ -14,6 +17,7 @@ from .managers import (
 
 
 class Eligibility(ObjectRoot):
+    # TODO: should be removed in the future?
     class Meta:
         verbose_name_plural = 'Eligibility'
         db_table = 'eligibility'
@@ -24,6 +28,7 @@ class Eligibility(ObjectRoot):
 
 
 class AgencyEligibilityConfig(ObjectRoot):
+    # TODO: should be removed in the future?
     class Meta:
         db_table = 'eligibility_agency_config'
         ordering = ['-created_at']
@@ -45,7 +50,7 @@ class ClientEligibility(ObjectRoot):
         editable=False
     )
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='eligibility')
-    eligibility = models.ForeignKey(Eligibility, on_delete=models.CASCADE)
+    eligibility = models.ForeignKey(Eligibility, on_delete=models.CASCADE)  # TODO: should be removed in the future?
     status = models.CharField(
         max_length=32,
         choices=[(x.name, x.value) for x in EligibilityStatus]
@@ -55,15 +60,22 @@ class ClientEligibility(ObjectRoot):
 
     objects = ClientEligibilityObjectManager()
 
-    @staticmethod
-    def newest(cls, client):
-        cls.objects.filter(client=client).first()
+    @classmethod
+    def is_eligible(cls, client):
+        newest = cls.objects.filter(client=client).first()
+        # TODO refactor enums to TextChoice
+        is_eligible = newest is not None and newest.status == EligibilityStatus.ELIGIBLE.name
+        return is_eligible
 
 
 class EligibilityQueue(ObjectRoot):
     class Meta:
         db_table = 'eligibility_queue'
         ordering = ['-created_at']
+        constraints = [
+            models.UniqueConstraint(fields=['client', 'requestor'], condition=Q(
+                status=None), name='unique_eligilibity_request')
+        ]
 
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     requestor = models.ForeignKey(Agency, on_delete=models.CASCADE)
@@ -71,8 +83,23 @@ class EligibilityQueue(ObjectRoot):
         blank=True,
         null=True,
         max_length=32,
+        # TODO refactor enums https://adamj.eu/tech/2020/01/27/moving-to-django-3-field-choices-enumeration-types/
         choices=[(x.name, x.value) for x in EligibilityStatus]
     )
-    resolved_by = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
+    resolved_by = models.ForeignKey(User, on_delete=models.PROTECT, blank=True,
+                                    null=True, related_name='resolved_eligibility')
+
+    @property
+    def is_resolved(self):
+        return self.status is not None
 
     objects = EligibilityQueueObjectManager()
+
+
+@receiver(post_save, sender=EligibilityQueue)
+def update_client_eligibility(sender, instance, created, **kwargs):
+    if instance._meta.model.objects.first() == instance and instance.is_resolved:
+        instance.client.eligibility.create(
+            status=instance.status,
+            eligibility=Eligibility.objects.first()  # should be removed in the future?
+        )
