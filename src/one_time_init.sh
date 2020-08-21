2ed3b64502b9ea73549802396d53efcbd70ed0c2
Przemysław Górecki <przemyslaw@toptal.com>
2020-08-21 16:52:45 +0200

    add one time init script

---
+++ src/one_time_init.sh
@@ -0,0 +1,1 @@
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.count() == 0 and User.objects.create_superuser('admin@ctagroup.org', 'admin@myproject.com', 'password')" | python manage.py shell
