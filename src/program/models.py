3efcde96cb4582010a5ad068fd9148fa1a3752e0
Mehmet Turanalp <mehmet@ctagroup.org>
2020-09-03 12:20:28 -0700

    MPR import model changes
    MPR import related model.py changes : iep\models.py and program\models.py

--- src/program/models.py
+++ src/program/models.py
@@ -48,7 +48,7 @@ class Enrollment(ObjectRoot):
     start_date = models.DateField(blank=True, null=True)
     projected_end_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    end_reason = models.CharField(max_length=200,blank=True, null=True,description='MPR file column:Reason Participation Terminated')   #MPR
+    end_reason = models.CharField(max_length=200,blank=True, null=True,help_text='MPR file column:Reason Participation Terminated')   #MPR
     status = models.CharField(
         max_length=32,
         choices=[(x.name, x.value) for x in EnrollmentStatus]
@@ -84,15 +84,7 @@ class ProgramEligibility(ObjectRoot):
 
     objects = ProgramEligibilityObjectManager()
 
-class AttendanceWeek(models.model):                                                      #MPR
-    class Meta:                                                                          #MPR
-        db_table = 'program_enrollment_activity_attendanceweek'                          #MPR
-        ordering = ['id']                                                                #MPR
-    week = models.CharField(max_length=10,blank=True, null=True,description='MPR file column:Actual Attendance Week - week')   #MPR 
-    hours = models.DecimalField(max_digits = 5, decimal_places = 2,null=True, description='MPR file column:Actual Attendance Week - hours')   #MPR
-    activity = ForeignKey(EnrollmentActivity, on_delete=models.CASCADE, related_name='attendance_weeks')  #MPR
-    
-class EnrollmentActivity(models.Model): 
+class EnrollmentActivity(ObjectRoot): 
     class Meta:
         db_table = 'program_enrollment_activity'
         ordering = ['id']
@@ -100,28 +92,37 @@ class EnrollmentActivity(models.Model):
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE, related_name='activities')
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
-    qualifying_activity_name = models.CharField(max_length=64,blank=True, null=True,description='MPR file column:Qualifying Activity Enrolled')   #MPR 
-    qualifying_activity_hours = models.IntegerField(blank=True, null=True,description='MPR file column:Qualifying Activity (51% or >) Hours')   #MPR
-    billable_activity = models.CharField(max_length=6,blank=True, null=True,description='MPR file column:Is Qualifying Activity Billable (Y/N)')   #MPR
-    non_qualifying_activity_hours = models.IntegerField(blank=True, null=True,description='MPR file column:Non-Qualifying Activity (49% or <) Hours')   #MPR
-    required_number_of_articipatio_hours = models.IntegerField(blank=True, null=True,description='MPR file column:Required Number of Participation Hours')   #MPR
-    actual_total_monthly_participation_hours = models.DecimalField(max_digits = 5, decimal_places = 2,null=True, description='MPR file column:Actual Total Monthly Participation Hours')   #MPR
-    hours_met = models.CharField(max_length=6,blank=True, null=True,description='MPR file column:Was ABAWD Work Requirement Met (Y/N)')   #MPR
-    performance_met = models.CharField(max_length=6,blank=True, null=True,description='MPR file column:Meeting Performance Standards (Y/N)')   #MPR
+    qualifying_activity_name = models.CharField(max_length=64,blank=True, null=True,help_text='MPR file column:Qualifying Activity Enrolled')   #MPR 
+    qualifying_activity_hours = models.IntegerField(blank=True, null=True,help_text='MPR file column:Qualifying Activity (51% or >) Hours')   #MPR
+    billable_activity = models.CharField(max_length=6,blank=True, null=True,help_text='MPR file column:Is Qualifying Activity Billable (Y/N)')   #MPR
+    non_qualifying_activity_hours = models.IntegerField(blank=True, null=True,help_text='MPR file column:Non-Qualifying Activity (49% or <) Hours')   #MPR
+    required_number_of_articipatio_hours = models.IntegerField(blank=True, null=True,help_text='MPR file column:Required Number of Participation Hours')   #MPR
+    actual_total_monthly_participation_hours = models.DecimalField(max_digits = 5, decimal_places = 2,null=True, help_text='MPR file column:Actual Total Monthly Participation Hours')   #MPR
+    hours_met = models.CharField(max_length=6,blank=True, null=True,help_text='MPR file column:Was ABAWD Work Requirement Met (Y/N)')   #MPR
+    performance_met = models.CharField(max_length=6,blank=True, null=True,help_text='MPR file column:Meeting Performance Standards (Y/N)')   #MPR
     # MPR 'If not Meeting Performance Standards, Please Explain' column will be stored in Note model
-    month = models.CharField(max_length=20,null=False,default='October 1900',description='MPR month or period')   #MPR
-    sheet = models.CharField(max_length=20,null=False,default='October 1900',description='MPR sheet name')   #MPR
-    
-    # We need timestamp field?
+    month = models.CharField(max_length=20,blank=True, null=True,default='Service Month:  ',help_text='MPR month or period')   #MPR
+    sheet = models.CharField(max_length=20,blank=True, null=True,default='October 1900',help_text='MPR sheet name')   #MPR
+    provider = models.CharField(max_length=20,blank=True, null=True,default='(Provider Name)',help_text='MPR sheet name')   #MPR
+    data_import_id = models.CharField(max_length=36,blank=True, null=True,help_text='MPR import job run instance')   #MPR
 
-class EnrollmentService(models.Model):
+class AttendanceWeek(models.Model):                                                      #MPR
+    class Meta:                                                                          #MPR
+        db_table = 'program_enrollment_activity_attendanceweek'                          #MPR
+        ordering = ['id']                                                                #MPR
+    week = models.CharField(max_length=10,blank=True, null=True,help_text='MPR file column:Actual Attendance Week - week')   #MPR 
+    hours = models.DecimalField(max_digits = 5, decimal_places = 2,null=True, help_text='MPR file column:Actual Attendance Week - hours')   #MPR
+    activity = models.ForeignKey(EnrollmentActivity, on_delete=models.CASCADE, related_name='attendance_weeks')  #MPR
+    
+class EnrollmentService(ObjectRoot):
     class Meta:
         db_table = 'program_enrollment_service'
         ordering = ['id']
     enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE, related_name='services')
     effective_date = models.DateField(blank=True, null=True)
-    offered = models.CharField(max_length=6,blank=True, null=True,description='MPR file column:Support Service Offered (Y/N)')   #MPR
-    type_and_amount = models.CharField(max_length=64,blank=True, null=True,description='MPR file column:Support Service Issued (Type & Amount)')   #MPR
-    if_no_support_services_needed_explain_why = models.CharField(max_length=200,blank=True, null=True,description='MPR file column:If No Support Services Needed, Explain Why')   #MPR
-    retention_services_type_amount = models.CharField(max_length=64,blank=True, null=True,description='MPR file column:Retention Services Provided (Type & Amount)')   #MPR
+    offered = models.CharField(max_length=6,blank=True, null=True,help_text='MPR file column:Support Service Offered (Y/N)')   #MPR
+    type_and_amount = models.CharField(max_length=64,blank=True, null=True,help_text='MPR file column:Support Service Issued (Type & Amount)')   #MPR
+    if_no_support_services_needed_explain_why = models.CharField(max_length=200,blank=True, null=True,help_text='MPR file column:If No Support Services Needed, Explain Why')   #MPR
+    retention_services_type_amount = models.CharField(max_length=64,blank=True, null=True,help_text='MPR file column:Retention Services Provided (Type & Amount)')   #MPR
     # MPR Comments column will be stored in Note model
+    data_import_id = models.CharField(max_length=36,blank=True, null=True,help_text='MPR import job run instance')   #MPR
