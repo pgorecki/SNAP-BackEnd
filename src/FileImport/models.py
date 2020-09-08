d4bf661a31df0698c38dd3ddd58be95a2fb1caa9
Mehmet Turanalp <mehmet@ctagroup.org>
2020-09-08 14:20:48 -0700

    FileImport scripts
    Started FileImport scripts with MPR file import

--- src/FileImport/models.py
+++ src/FileImport/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from .enums import FileImportTypes
+from program.enums import EnrollmentStatus
 from django.contrib.auth.models import User    #MPR
 
 import pandas as pd
 import xlrd 
-from program.models import Enrollment
+from program.models import Enrollment, Program
 from client.models import Client, ClientAddress
 import logging
 logging.basicConfig(filename = 'MPRapp.log', level = logging.INFO)
 from django.core.exceptions import MultipleObjectsReturned
+from iep.models import ClientIEP
+from datetime import date
+from agency.models import Agency
 
 class FileImport(models.Model):                                                                          #MPR
     ftype = models.CharField( max_length=32, blank=False, null=False, help_text='Excel File Type Import:', #MPR
@@ -17,6 +21,7 @@ class FileImport(models.Model):                                                 
     user = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True,help_text='User executing the import')                #MPR
     import_parameters = models.CharField(max_length=200,blank=True, null=True,help_text='parameters of the import is a dictionary stored as json.loads string')   #MPR
     period = models.CharField(max_length=36,blank=True, null=True,help_text='file upload period in weeks or months etc..')   #MPR
+    agency = models.ForeignKey(Agency, on_delete=models.PROTECT, blank=True, null=True,help_text='Agency for which the import is executed, typically User\'s agency.')  
     
     def inspect(self):                                                                             #MPR
         # Inspect the input file for format validation                                             #MPR
@@ -45,32 +50,95 @@ class FileImport(models.Model):                                                 
            df = pd.read_excel(self.file_path, sheet_name=1, header=10)
         except IOError as e:
            logging.exception(str(e))
-           return (0,0,0,'Error reading excel file!')       
+           return (0,0,0,'Error reading excel file!')            
+        a3=Agency.objects.get_or_create(name=provider)
         df.dropna(how = 'all',inplace=True)
         print(df.columns.values)
         df1=df.iloc[0:18,]   ## TODO use ix first rows to restrict number of records processed
         df1.rename(columns={'Unnamed: 15': 'Actual Attendance Week Hours'}, inplace=True)
-        print(df1)  ## TODO delete later
+        df1[' Client ID'].astype('Int64', copy=False)
+        df1['Case Number'].astype('Int64', copy=False)
+        #print(df1)  ## TODO delete later
+        print(df1.info())
         insert_count=0
         fail_count=0
         attempt_count=0
         for i in range(0, df1.shape[0]): # for each row in the excel file data section
           row = df1.iloc[i]
           if not(pd.isnull(row['First Name'])) or not(pd.isnull(row['Last Name'])) or not(pd.isnull(row[' Client ID'])):   #if any one of them is non-null
-            # save record
             # par=participation(First_Name=row['First Name'],Last_Name=row['Last Name'],...)
             try:
                c3=Client.objects.get(snap_id=row[' Client ID'])
-            except MultipleObjectsReturned:
-               print('MultipleObjectsReturned . Picking the first one.')
-               c3=Client.objects.filter(snap_id=row[' Client ID']).first()  
+               print('Client in the DB:' + str(c3.first_name) + ' ' + str(c3.last_name) + ' with pk=' + str(c3.id) )
+               if c3.address.county!=row['County of Residence']:
+                  c3.address.county=row['County of Residence']
+                  c3.address.save()
             except Client.DoesNotExist:
-               print('Client Does not exits. Create One') 
                ca1=ClientAddress(county=row['County of Residence'])
                c3=Client(first_name=row['First Name'],last_name=row['Last Name'],snap_id=row[' Client ID'],address=ca1)
+               ca1.save()
+               c3.save()
+               print('Created Client' + str(c3.first_name) + ' ' + str(c3.last_name) + ' with pk=' + str(c3.id) )
+            except MultipleObjectsReturned:
+               c3=Client.objects.filter(snap_id=row[' Client ID']).first() 
+               print('MultipleObjectsReturned for gsnap_id=' + str(c3.snap_id) + '. Picking the first one with pk=' + str(c3.pk))     
+            ## TODO: Do we update c3.address.county?             
+            if c3.ieps.count():
+                  ciep3=c3.ieps.filter(case_number=row['Case Number']).first()
+                  if not ciep3:
+                     ciep3=ClientIEP(client=c3, case_number=row['Case Number'],projected_end_date=row['Projected End Date '].date())
+                     print('Created ClientIEP with case number=' +  str(ciep3.case_number) + ' with pk=' + str(ciep3.id) )   
+                  else:
+                     print('Found ClientIEP with case_number=' + str(ciep3.case_number) )                  
+                     ciep3.projected_end_date=row['Projected End Date '].date()
+            else:
+                  ciep3=ClientIEP(client=c3, case_number=row['Case Number'],projected_end_date=row['Projected End Date '].date())
+                  print('Created ClientIEP with case number=' +  str(ciep3.case_number) + ' with pk=' + str(ciep3.id) )                
+            ciep3.save()                  
+            if ciep3.iep_enrollments.count():
+                  ciep_en3=ciep3.iep_enrollments.first()
+            else:
+                  ciep_en3=ClientIEPEnrollment(iep=ciep3)
+                  ciep_en3.save()
+            if ciep_en3.enrollment and ciep_en3.enrollment.start_date == row['Activity Enrollment Date'].date():
+                  print('Enrollment exists')
+                  e3=ciep_en3.enrollment
+                  e3.end_date=row['Date Participation Terminated'].date()
+                  e3.end_reason=row['Reason Participation Terminated']
+                  e3.save()
+            else:
+                  print('Creating enrollment object')
+                  p3=Program.objects.filter(name=row['Qualifying Activity Enrolled']).first()
+                  if not p3:
+                     print('Creating program')
+                     p3=Program(name=row['Qualifying Activity Enrolled'],agency=a3)
+                     p3.save()
+                  e3=Enrollment(client=c3,program=p3,start_date=row['Activity Enrollment Date'].date(), end_date=row['Date Participation Terminated'].date(), end_reason=row['Reason Participation Terminated'],status=EnrollmentStatus.ENROLLED.value)
+                  e3.save()
+                  ciep_en3.enrollment=e3
+                  ciep_en3.save()
+
+                  
+                         
+                      
+            # try:
+               # ciep3=ClientIEP.objects.get(case_number=row['Case Number'])
+               # print('IEPClient in the DB: with pk=' + str(ciep3.id) )
+            # except ClientIEP.DoesNotExist:
+               # ciep3=ClientIEP(client=c3, case_number=row['Case Number'])
+               # ciep3.save()
+               # print('Created Client' + str(c3.first_name) + ' ' + str(c3.last_name) + ' with pk=' + str(c3.id) )
+            # except MultipleObjectsReturned:
+               # ciep3=ClientIEP.objects.filter(case_number=row['Case Number']).first() 
+               # print('MultipleObjectsReturned for Case Number=' + str(ciep3.case_number) + '. Picking the first one with pk=' + str(ciep3.pk))    
+ 
+   #if Case Number does not exist:
+   #   iep1=create ClientIEP(Case Number, Projected End Date,  ABAWD (Y/N) )
+   #else:
+   #   iep1= get ClientIEP(Case Number)
+
+           
             attempt_count+=1
-            ca1.save()
-            c3.save()
             if (c3.id):
                insert_count+=1
             else:
