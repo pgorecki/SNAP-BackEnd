a62210c62ac01538d80d182331d3cd74d6a7e650
Przemysław Górecki <przemyslaw.gorecki@gmail.com>
2020-09-21 10:57:20 +0200

    remove client address from admin

--- src/client/admin.py
+++ src/client/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from client.models import Client, ClientAddress
+from client.models import Client
 from agency.admin import AgencyClientInline
-
-
-admin.site.register(ClientAddress)
 
 
 @admin.register(Client)
