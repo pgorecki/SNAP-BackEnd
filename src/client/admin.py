57820179fbe3868bef0300b039173756a0a087b3
Przemysław Górecki <przemyslaw@toptal.com>
2020-09-24 14:08:06 +0200

    change direction of one2one relation

--- src/client/admin.py
+++ src/client/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from client.models import Client
+from client.models import Client, ClientAddress
 from agency.admin import AgencyClientInline
+
+
+class ClientAddressInline(admin.StackedInline):
+    model = ClientAddress
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ("id", "first_name", "middle_name", "last_name", "address")
+    list_display = ("id", "first_name", "middle_name", "last_name")
 
-    inlines = (AgencyClientInline,)
-
-    def get_queryset(self, request):
-        return Client.objects.for_user(request.user)
+    inlines = (AgencyClientInline, ClientAddressInline)
--- src/client/models.py
+++ src/client/models.py
@@ -3,22 +3,6 @@ from core.models import ObjectRoot
 from core.managers import AgencyObjectManager
 
 
-class ClientAddress(models.Model):
-    class Meta:
-        db_table = "client_address"
-        verbose_name = "Client Address"
-        verbose_name_plural = "Client Addresses"
-
-    street = models.CharField(max_length=256, blank=True, default="")
-    city = models.CharField(max_length=64, blank=True, default="")
-    state = models.CharField(max_length=64, blank=True, default="")
-    zip = models.CharField(max_length=8, blank=True, default="")
-    county = models.CharField(max_length=64, blank=True, default="")
-
-    def __str__(self):
-        return f"{self.street} {self.city} {self.state} {self.zip}"
-
-
 class Client(ObjectRoot):
     class Meta:
         db_table = "client"
@@ -42,9 +26,6 @@ class Client(ObjectRoot):
     is_new = models.BooleanField(default=True)
 
     snap_id = models.CharField(max_length=256, blank=True, null=True)
-    address = models.OneToOneField(ClientAddress, on_delete=models.CASCADE, null=True)
-
-    objects = AgencyObjectManager()
 
     @property
     def full_name(self):
@@ -53,3 +34,20 @@ class Client(ObjectRoot):
 
     def __str__(self):
         return self.full_name
+
+
+class ClientAddress(models.Model):
+    class Meta:
+        db_table = "client_address"
+        verbose_name = "Client Address"
+        verbose_name_plural = "Client Addresses"
+
+    client = models.OneToOneField(Client, null=True, on_delete=models.CASCADE, related_name='address')
+    street = models.CharField(max_length=256, blank=True, default="")
+    city = models.CharField(max_length=64, blank=True, default="")
+    state = models.CharField(max_length=64, blank=True, default="")
+    zip = models.CharField(max_length=8, blank=True, default="")
+    county = models.CharField(max_length=64, blank=True, default="")
+
+    def __str__(self):
+        return f"{self.street} {self.city} {self.state} {self.zip}"
